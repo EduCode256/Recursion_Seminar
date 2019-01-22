6ee1d074a2eea6e87894b161996607ff77e78251
N4N0CH1P <mariano.hmc@hotmail.com>
2019-01-22 17:34:32 -0600

    Adding Exercise1

---
+++ Exercise1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int graph[5][5] = {{0,1,1,0,0},
+				   {0,0,0,1,0},
+				   {0,0,0,0,1},
+				   {1,1,0,0,0},
+				   {1,0,1,0,0}};
+void DFS(int origin){
+	//WRITE YOUR CODE HERE
+}
+
+int main(int argc, char *args[]){
+	//TIP: Try DFS with every node (0-4)
+	DFS(0);
+	return(0);
+}
